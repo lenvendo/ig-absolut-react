228d231e4a1abf6dec3e20a4a01231d3f1ac8764
Kirill Vanyukov <vanyukovkirill@gmail.com>
2021-08-16 14:00:30 +0300

    Character

---
+++ src/Components/Character.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Tooltip from "./Tooltip";
+import "./Character.css";
+
+const Character = ({ character }) => (
+  <div className="character">
+    <img src={character.image} alt={character.name} />
+    <Tooltip item={character} className="character__tooltip" />
+  </div>
+);
+
+Character.propTypes = {
+  character: PropTypes.object,
+};
+
+export default Character;
